5277145cb2ca6763c873dc1235c01614a721d53c
David Omisakin <davidomisakin4good@gmail.com>
2023-03-06 21:56:18 +0100

    done

--- signup.js
+++ signup.js
@@ -22,3 +22,13 @@ myform.addEventListener("submit", function (e) {
     }
     localStorage.setItem("users", JSON.stringify(user))
 })
+
+
+function togglePasswordVisibility() {
+    var password = document.getElementById("password");
+    if (password.type === "password") {
+      password.type = "text";
+    } else {
+      password.type = "password";
+    }
+  }
